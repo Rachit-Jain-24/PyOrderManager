da32177b8fa8896251519e1b4643229e49514b82
Rachit Jain <Rachit24jain@gmail.com>
2024-06-08 00:56:12 +0530

    Imported libraries required

---
+++ app.py
@@ -0,0 +1,16 @@
+'''
+____________________________________________________________________________________________
+
+'''
+# Libraries used for Retail Order Manager:
+
+from openpyxl import Workbook, load_workbook
+from datetime import datetime
+import os.path
+import pandas as pd
+
+
+'''
+____________________________________________________________________________________________
+
+'''
