1ede28caa034c0434e3c5c5cb95469006b615dfa
Rachit Jain <Rachit24jain@gmail.com>
2024-06-08 19:09:55 +0530

    added Error Handling for file name input

--- app.py
+++ app.py
@@ -15,27 +15,16 @@ ________________________________________________________________________________
 
 '''
 
-file_path=str(input("enter the file name you want to open or create as a new file: ")+".xlsx")
 
-# Check if the Excel file exists
-if os.path.isfile(file_path):
-    workbook = load_workbook(file_path)
-    sheet = workbook.active
-else:
-    # Create a new Excel workbook
-    workbook = Workbook()
-    sheet = workbook.active
+print("--------Welcome to PyRetail Manager--------")
 
-    # Define column headers
-    sheet['A1'] = 'Order ID'
-    sheet['B1'] = 'Product Name'
-    sheet['C1'] = 'Price'
-    sheet['D1'] = 'Quantity'
-    sheet['E1'] = 'Total Amount'
-    sheet['F1'] = 'Customer Name'
-    sheet['G1'] = 'Phone Number'
-    sheet['H1'] = 'Email'
-    sheet['I1'] = 'Order Date & Time'
+# Input file name with error handling
+while True:
+    file_path = input('\n'"Enter the file name you want to open or create (with .xlsx): ")
+    if file_path.endswith(".xlsx"):
+        break
+    else:
+        print("Invalid file name. Please include '.xlsx' extension.")
 
 '''
 ____________________________________________________________________________________________
