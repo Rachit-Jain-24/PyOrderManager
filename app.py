d61b6a9bfab30821ac1597ff692dc3fe2a1403bf
Rachit Jain <Rachit24jain@gmail.com>
2024-06-08 19:15:28 +0530

    add_order function

--- app.py
+++ app.py
@@ -74,3 +74,49 @@ def create_excel():
 ____________________________________________________________________________________________
 
 '''
+
+# Function to add a new order
+def add_order():
+#    Adds a new order to the Excel file.
+#    This function prompts the user to enter the details of the order, including the order ID, product name, price, quantity, customer name, phone number, and email. The function then calculates the total amount of the order by multiplying the price and quantity. The current date and time is also recorded.
+#    The function appends the order details to the 'orders_3.xlsx' Excel file. The file is saved after adding the order.
+#    This function does not take any parameters.
+#    This function does not return any value.
+
+    order_id = input("Enter Order ID: ")
+    product_name = input("Enter Product Name: ")
+
+    while True:
+        try:
+            price = float(input("Enter Price: "))
+            if price <= 0:
+                raise ValueError("Price must be a positive number.")
+            break  # Exit the loop if input is valid
+        except ValueError as e:
+            print(f"Invalid input: {e}. Please enter a valid positive number.")
+
+    while True:
+        try:
+            quantity = int(input("Enter Quantity: "))
+            if quantity <= 0:
+                raise ValueError("Quantity must be a positive integer.")
+            break
+        except ValueError as e:
+            print(f"Invalid input: {e}. Please enter a valid positive integer.")
+
+    total_amount = price * quantity
+    customer_name = input("Enter Customer Name: ")
+    phone_number = input("Enter Phone Number: ")
+    email = input("Enter Email: ")
+    date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # writing the data into excel file under the column heads
+    sheet.append([order_id, product_name, price, quantity, total_amount, customer_name, phone_number, email, date_time])
+    workbook.save(file_path)
+    print("Order added successfully!"'\n')
+
+'''
+____________________________________________________________________________________________
+
+'''
+
